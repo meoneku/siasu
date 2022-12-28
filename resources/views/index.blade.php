32c82865c63822169c6076eb7ed59eafc73b2dd1
meoneku <virmon41@gmail.com>
2022-12-28 10:34:29 +0700

    Change Front

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,100 +2,109 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Si 🐶</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ url('plugins/fontawesome-free/css/all.min.css') }}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ url('css/adminlte.min.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+
+    <title>👩🏽‍🚀</title>
 </head>
 
-<body class="hold-transition layout-top-nav">
-    <div class="wrapper">
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-
-            <!-- Right navbar links -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Notifications Dropdown Menu -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <i class="far fa-bell"></i>
-                        <span class="badge badge-warning navbar-badge">New</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <span class="dropdown-item dropdown-header">New</span>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-envelope mr-2"></i> 4 new messages
-                            <span class="float-right text-muted text-sm">3 mins</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                        <i class="fas fa-expand-arrows-alt"></i>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.navbar -->
-
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Main content -->
-            <section class="content">
-                <div class="container">
-                    <h2 class="text-center display-4">Google Mini</h2>
-                    <div class="row">
-                        <div class="col-md-8 offset-md-2">
-                            <form action="simple-results.html">
-                                <div class="input-group">
-                                    <input type="search" class="form-control form-control-lg" placeholder="Apa Yang Anda Cari?">
-                                    <div class="input-group-append">
-                                        <button type="submit" class="btn btn-lg btn-default">
-                                            <i class="fa fa-search"></i>
-                                        </button>
+<body class="p-3 mb-2 bg-success text-white">
+    <main class="container">
+        <div class="text-left">
+            <div class="row">
+                <div class="col-md-4"><img src="img/UNHASY.png" class="rounded" alt="UNHASY" width="32px" height="32px" /> <span class="navbar-brand mb-0 h1">Fakultas Teknik</span></div>
+                <div class="col-md-4 ms-auto d-flex justify-content-end align-items-center"><span class="navbar-brand h1">Sistem Informasi 🐶</span></div>
+            </div>
+        </div>
+        <div class="mt-3">
+            <div class="card">
+                <div class="card-body text-black">
+                    <div class="container text-center">
+                        <div class="row">
+                            <div class="col-3">
+                                <div class="card" data-tilt>
+                                    <a href="#"><img src="img/svg/business-man-employee-general-svgrepo-com.svg" class="card-img-top" alt="SIP!" height="150px" width="150px" /></a>
+                                    <div class="card-body">
+                                        <p class="h6">Pendaftaran Magang</p>
                                     </div>
                                 </div>
-                            </form>
+                            </div>
+                            <div class="col-3">
+                                <div class="card" data-tilt>
+                                    <a href="#"><img src="img/svg/document-general-letter-3-svgrepo-com.svg" class="card-img-top" alt="SIP!" height="150px" width="150px" /></a>
+                                    <div class="card-body">
+                                        <p class="h6">Pendaftaran Skripsi</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card" data-tilt>
+                                    <a href="#"><img src="img/svg/document-general-letter-svgrepo-com.svg" class="card-img-top" alt="SIP!" height="150px" width="150px" /></a>
+                                    <div class="card-body">
+                                        <p class="h6">Pendaftaran Seminar</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card" data-tilt>
+                                    <a href="#"><img src="img/svg/document-general-letter-2-svgrepo-com.svg" class="card-img-top" alt="SIP!" height="150px" width="150px" /></a>
+                                    <div class="card-body">
+                                        <p class="h6">Pendaftaran Seminar Hasil</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Surat -->
+                        <div class="row mt-3">
+                            <div class="col-3">
+                                <div class="card" data-tilt>
+                                    <a href="#"><img src="img/svg/brochure-document-menu-svgrepo-com.svg" class="card-img-top" alt="SIP!" height="150px" width="150px" /></a>
+                                    <div class="card-body">
+                                        <p class="h6">Surat Izin Magang</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card" data-tilt>
+                                    <a href="#"><img src="img/svg/bag-briefcase-business-bag-svgrepo-com.svg" class="card-img-top" alt="SIP!" height="150px" width="150px" /></a>
+                                    <div class="card-body">
+                                        <p class="h6">Surat Izin Penelitian</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card" data-tilt>
+                                    <a href="#"><img src="img/svg/folder-office-organize-svgrepo-com.svg" class="card-img-top" alt="SIP!" height="150px" width="150px" /></a>
+                                    <div class="card-body">
+                                        <p class="h6">Surat Keterangan Mahasiswa</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card" data-tilt>
+                                    <a href="#"><img src="img/svg/page-general-document-svgrepo-com.svg" class="card-img-top" alt="SIP!" height="150px" width="150px" /></a>
+                                    <div class="card-body">
+                                        <p class="h6">Yudisium</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </section>
-        </div>
-
-        <!-- Main footer -->
-        <footer class="main-footer">
-            <!-- To the right -->
-            <div class="float-right d-none d-sm-inline">
-
             </div>
-            <!-- Default to the left -->
-            <strong>Copyright &copy; 2022 <a href="#">Boy Studio</a>.</strong> All rights reserved.
+        </div>
+        <footer class="mt-3">
+            <div class="d-flex justify-content-end">
+                <p>🐶 <span class="text-muted fst-italic fw-semibold">&#169; 2023 Fakultas Teknik Universitas Hasyim Asy'ari Tebuireng Jombang</span></p>
+            </div>
         </footer>
-
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
-    </div>
-    <!-- ./wrapper -->
-
-    <!-- jQuery -->
-    <script src="{{ url('plugins/jquery/jquery.min.js') }}"></script>
-    <!-- Bootstrap 4 -->
-    <script src="{{ url('plugins/bootstrap/bootstrap.bundle.min.js') }}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{ url('js/adminlte.min.js') }}"></script>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.2.1/tilt.jquery.min.js"></script>
 </body>
 
 </html>
